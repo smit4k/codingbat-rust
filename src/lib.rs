98797a4ad0b09e30f24d92467acbb349933b6f83
Smit Patil <smit@smit.codes>
2026-05-12 10:43:43 -0400

    Fix doctests by using modules in lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod array1;
+pub mod array2;
+pub mod logic1;
+pub mod warmup1;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-mod array1;
-mod array2;
-mod logic1;
-mod warmup1;
-
 fn main() {
     println!("Hello, world!");
 }
